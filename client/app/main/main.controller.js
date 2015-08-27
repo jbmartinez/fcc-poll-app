aa7af919d2b14dc3f83f1015d1374b894f55fd72
Juan Martínez <jbmartinezs@yahoo.com>
2015-08-27 13:38:20 +0000

    more things deletions

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,22 +7,4 @@ angular.module('workspaceApp')
         $location.path('/dashboard');
       }
     });
-
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
   });
