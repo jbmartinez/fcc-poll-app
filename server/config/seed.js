4d2050f3a982e9e78c1830dadba6f0cfcffb2cbe
Juan Martínez <jbmartinezs@yahoo.com>
2015-08-27 18:13:11 +0000

    seed some data in production

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,49 +5,41 @@
 
 'use strict';
 
-// var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
 var Poll = require('../api/poll/poll.model');
 
-/* Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-  }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
+// do not seed admin user if we're in production
+if (process.env.NODE_ENV === 'production') {
+  User.find({}).remove(function() {
+    User.create({
+      provider: 'local',
+      name: 'Test User',
+      email: 'test@test.com',
+      password: 'test'
+    }, function() {
+        console.log('finished populating users');
+      }
+    );
   });
-}); */
-
-User.find({}).remove(function() {
-  User.create({
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function() {
-      console.log('finished populating users');
-    }
-  );
-});
+} else {
+  User.find({}).remove(function() {
+    User.create({
+      provider: 'local',
+      name: 'Test User',
+      email: 'test@test.com',
+      password: 'test'
+    }, {
+      provider: 'local',
+      role: 'admin',
+      name: 'Admin',
+      email: 'admin@admin.com',
+      password: 'admin'
+    }, function() {
+        console.log('finished populating users');
+      }
+    );
+  });
+}
 
 Poll.find({}).remove(function() {
   Poll.create({
@@ -62,6 +54,16 @@ Poll.find({}).remove(function() {
     options: [
       {text: 'of course!', count: 45},
       {text: 'definitely!', count: 30}
+    ]
+  }, {
+    question: 'Whats your favorite programming language?',
+    options: [
+      {text: 'JavaScript', count: 40},
+      {text: 'ruby', count: 10},
+      {text: 'python', count: 35},
+      {text: 'lua', count: 15},
+      {text: 'C', count: 7},
+      {text: 'PHP', count: 21}
     ]
   });
 });
